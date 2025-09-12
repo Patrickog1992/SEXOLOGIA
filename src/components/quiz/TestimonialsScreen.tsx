f911bc9e814ca4420884f3bde66fd6d90cfeead3
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-12 17:14:52 +0000

    ARRUME LOGO ISSO CARALHO

--- src/components/quiz/TestimonialsScreen.tsx
+++ src/components/quiz/TestimonialsScreen.tsx
@@ -44,13 +44,13 @@ export default function TestimonialsScreen({ onNext }: TestimonialsScreenProps) 
             </h2>
 
             <p className="text-lg sm:text-xl font-bold text-primary">Veja o depoimento do Pedro Paulo</p>
-            <div style={{position: 'relative', width: '100%', height: 0, paddingBottom: '177.78%'}}>
+            <div className="aspect-w-9 aspect-h-16 w-full">
               <iframe src="https://www.youtube.com/embed/QadrzlO0H3k?rel=0&controls=1&modestbranding=1"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
-                style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
+                className="absolute top-0 left-0 w-full h-full">
               </iframe>
             </div>
 
